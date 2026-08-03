680007b9f69e54c88d45690ac736b6e157859562
Sanjida Aktar <sanjidabristy555@gmail.com>
2026-08-04 01:40:50 +0600

    Add updated animation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,7 +23,6 @@ export default function Navbar() {
           Sanjida<span className="text-cyan-400">.</span>
         </a>
 
-        {/* Desktop Menu */}
         <div className="hidden items-center gap-8 md:flex">
           {links.map((link) => (
             <a
@@ -43,7 +42,6 @@ export default function Navbar() {
           Let's Talk
         </a>
 
-        {/* Mobile Button */}
         <button
           onClick={() => setMenuOpen(!menuOpen)}
           className="text-white md:hidden"
